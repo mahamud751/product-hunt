d353eb2cb318fa48ea4e6216410eea7da3c43759
Mahamud Pino <mahamudpino24678@gmail.com>
2025-02-19 03:18:42 +0600

    promos and alternatives

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,10 @@
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
- 
-export default { providers: [GitHub, Google] } satisfies NextAuthConfig
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
+
+const config: NextAuthConfig = {
+  providers: [GitHub, Google],
+  trustHost: true,
+};
+
+export default config;
